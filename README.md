994bbc6fe70f4b404603b1186520be6fee70a904
Benjamin Champetier <benjamin.champetier@gmail.com>
2018-11-08 13:20:56 +0100

    add kotlin-stdlib for the http request

---
+++ README.md
@@ -0,0 +1,9 @@
+# A Weather App made with Java Swing
+
+## Don't forget to add the kotlin stdlib and stdlib-common dependencies to your build
+https://jar-download.com/artifact-search/kotlin-stdlib
+
+## Libraries
+OkHttp http://square.github.io/okhttp/
+
+OkIo https://github.com/square/okio
--- src/com/splinter/weatherforcast/MainFrame.java
+++ src/com/splinter/weatherforcast/MainFrame.java
@@ -2,6 +2,10 @@ package com.splinter.weatherforcast;
 
 import javax.swing.JFrame;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainFrame extends JFrame {
 	/**
 	 * 
@@ -10,5 +14,24 @@ public class MainFrame extends JFrame {
 
 	public MainFrame(String title) {
 		super(title);
+		
+		String apiKey = "9ddcbca98bab559410fd7bbdffc14e68";
+		double latitude = 37.8267;
+		double longitude = -122.4233;
+		
+		// String forcastUrl = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
+		String forecastUrl= String.format("https://api.darksky.net/forecast/%s/%f,%f", apiKey, latitude, longitude);
+		
+		OkHttpClient httpClient = new OkHttpClient();
+		Request request = new Request.Builder()
+			      .url(forecastUrl)
+			      .build();
+		  try {
+			  Response response = httpClient.newCall(request)
+					  .execute();
+			System.out.println(response.body().string());
+		} catch (Exception e) {
+			System.err.println("Error: " + e);
+		}
 	}
 }
